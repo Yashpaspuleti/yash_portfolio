2d63c6e84ccb67b3a8efa9f00836ad40b994394b
yashwanth kumar <87013174+Yashpaspuleti@users.noreply.github.com>
2025-11-05 17:27:10 +0530

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Yashwanth Pasupuleti.
+        &copy; 2025 Yashwanth Pasupuleti.
         <br />
         All rights reserved.
       </p>
